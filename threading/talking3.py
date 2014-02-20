1c4bd8a0fc5d6a9a294777e80c245beb9922285f
Agam Dua <agam.dua@contentflow.in>
2014-02-20 20:24:48 +0530

    Basic threaded program in execution

--- threading/talking3.py
+++ threading/talking3.py
@@ -4,3 +4,31 @@ Three rabbits A, B and C feel adventurous and go exploring the
 
 This is how they communicate.
 '''
+
+import threading, time
+
+class Rabbit(threading.Thread):
+    def __init__(self, message):
+        threading.Thread.__init__(self)
+        self.message = message
+    def run(self):
+        if self.message:
+            self.message = self.message + 1
+            print self.message
+            time.sleep(3)
+            self.run()
+        else:
+            print "No message"
+            self.message = 1
+            time.sleep(1)
+            self.run()
+        return
+
+r1 = Rabbit(7)
+r1.start()
+
+r2 = Rabbit(None)
+r2.start()
+
+r3 = Rabbit (500)
+r3.start()
