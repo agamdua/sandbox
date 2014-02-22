cb741df8b53b61563a86ff8d96c8d26f1067002c
Agam Dua <agam.dua@contentflow.in>
2014-02-22 20:28:53 +0530

    Added (broken): docstrings, mock data dict, queue()
    Not working.
    Do not think the queue is being passed to thread.
    This will require passing a queue to run(), however not working by passing the
    param to start()
    TODO: Re-read the docs.

--- threading/talking3.py
+++ threading/talking3.py
@@ -5,32 +5,53 @@ Three rabbits A, B and C feel adventurous and go exploring the
 This is how they communicate.
 '''
 
-import threading, time
+from Queue import Queue
+import threading
 
 class Rabbit(threading.Thread):
+    '''
+    Class for communicating Rabbit threads
+    '''
+
     def __init__(self, message, name):
+        '''
+        Constructor for rabbit object.
+
+        Thread params:
+            message: Stores the message which the thread is operating on
+            name: Name of the thread for humans to identify
+        '''
         threading.Thread.__init__(self)
         self.message = message
-        self.name = name
-    def run(self):
-        if self.message:
-            self.message = self.message + 1
-            print "{} says the message received is = {}".format(self.name,
-                                                                self.message)
-            time.sleep(3)
-            self.run()
-        else:
-            print "No message from {}".format(self.name)
-            self.message = 1
-            time.sleep(1)
-            self.run()
+        self.name = name # is this the same as thread.name?
+
+    def run(self): # Is this better as a separate target function?
+        '''
+        Adds message to queue if empty, else retrieves the message
+        and adds one to it, adding the new value to queue
+        '''
+        # while True:
+        item = msg_q.get()
+        if item is None:
+            msg_q.put(100)
+            msg_q.get() # Get value off FIFO queue, not necessarily 100
+
+        # Process item
+        item = item + 1
+        print item
+
+        # Add item to queue and mark task as done
+        msg_q.put(item)
+        msg_q.task_done()
         return
 
-r1 = Rabbit(750, "Adam")
-r1.start()
+rabbit_info = { "Al": None,
+                "Bo": 500,
+                "Calvin": 720,
+              }
 
-r2 = Rabbit(None, "Bo")
-r2.start()
+msg_q = Queue()
 
-r3 = Rabbit (500, "Calvin")
-r3.start()
+for k,v in rabbit_info.items():
+    print k
+    t = Rabbit(name=k, message=v).start()
