cba88d01bd93ea06a70f86699d7dfe764ef47f86
Agam Dua <agam.dua@contentflow.in>
2014-02-22 19:13:19 +0530

    Named the threads and improved output formatting

--- threading/talking3.py
+++ threading/talking3.py
@@ -8,27 +8,29 @@ This is how they communicate.
 import threading, time
 
 class Rabbit(threading.Thread):
-    def __init__(self, message):
+    def __init__(self, message, name):
         threading.Thread.__init__(self)
         self.message = message
+        self.name = name
     def run(self):
         if self.message:
             self.message = self.message + 1
-            print self.message
+            print "{} says the message received is = {}".format(self.name,
+                                                                self.message)
             time.sleep(3)
             self.run()
         else:
-            print "No message"
+            print "No message from {}".format(self.name)
             self.message = 1
             time.sleep(1)
             self.run()
         return
 
-r1 = Rabbit(7)
+r1 = Rabbit(750, "Adam")
 r1.start()
 
-r2 = Rabbit(None)
+r2 = Rabbit(None, "Bo")
 r2.start()
 
-r3 = Rabbit (500)
+r3 = Rabbit (500, "Calvin")
 r3.start()
